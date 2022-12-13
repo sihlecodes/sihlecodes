a4f1531a93be29f1659588df237227ed2d7a45f4
Siphesihle Mhlongo <sphem4code@gmail.com>
2022-12-13 19:29:08 +0200

    Made minor style optimizations

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -30,8 +30,6 @@ learning new skills.
         <h3>My Story</h3>
         <div>
           Ever since I was young I always new I wanted to do something related to technology; I found tech fascinating.
-          During my grade 3 year I was tasked to create a recycling awareness poster for my school and later on that year
-          I received the award for computer skills.
           <ReadMore>
             <p>
               Skipping forward a couple of years, when I was my grade 9 year I became curious about programming. I initially assumed that I needed to know binary and so I learnt how the binary number system works and how these numbers get converted into other characters using the ASCII character encoding scheme. I later on realized that learning binary was not getting me any closer to my goal of learning how to program. So, I went back to the drawing board and instead of assuming; this time I searched the internet asking the simple question "How do you program?". I went through a couple of web pages and eventually discovered the first programming language I learnt, python.
@@ -44,7 +42,7 @@ learning new skills.
 
         <h3>My Vision</h3>
 
-        In x years I want to be at Y place in life.
+        <p>In x years I want to be at Y place in life.</p>
 
         <h3>Check out some of my work</h3>
         <p>A quick word of warning: I haven't used my instagram for a while. 😅</p>
