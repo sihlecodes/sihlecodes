afaac0b8d17f13098f84899a72f9f21524c788d3
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-01-23 22:08:27 +0200

    Reworked contact styling structure

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -38,23 +38,23 @@ function ContactForm() {
   }
 
   return (
-    <form className="Contact-form" ref={form} onSubmit={sendEmail}>
-      <div className="Contact-form-field">
-        <label for="sender_name"><span class className="Contact-required-field">*</span> Your name <span className="Contact-required-field">Required</span></label>
-        <input className="Contact-text-input" type="text" name="sender_name" required="true" />
+    <form className="form" ref={form} onSubmit={sendEmail}>
+      <div className="field">
+        <label for="sender_name"><span class className="required-indicator">*</span> Your name <span className="required-indicator">Required</span></label>
+        <input className="text-input" type="text" name="sender_name" required="true" />
       </div>
 
-      <div className="Contact-form-field">
+      <div className="field">
         <label for="sender_email">Your email address</label>
-        <input className="Contact-text-input" type="text" name="sender_email" />
+        <input className="text-input" type="text" name="sender_email" />
       </div>
 
-      <div className="Contact-form-field">
-        <label for="sender_message"><span className="Contact-required-field">*</span> Message <span className="Contact-required-field">Required</span></label>
-        <textarea className="Contact-text-input" name="sender_message" required="true"></textarea>
+      <div className="field">
+        <label for="sender_message"><span className="required-indicator">*</span> Message <span className="required-indicator">Required</span></label>
+        <textarea className="text-input" name="sender_message" required="true"></textarea>
       </div>
 
-      <input className="App-button" type="submit" value={buttonText} />
+      <input type="submit" value={buttonText} />
     </form>
   );
 }
@@ -68,14 +68,14 @@ const ContactIconLink = ({ href, src, alt }) => (
 
 function Contact() {
   return (
-    <div>
+    <div className="Contact">
       <h2>Get In Touch</h2>
 
-      <div className="Contact-content">
+      <div className="content">
         <p>Want to get in contact with me? Send me an email.</p>
         <ContactForm />
 
-        <div className="Contact-links">
+        <div className="links">
           <ContactIconLink href={Links.instagram} src={instagramIcon} />
           <ContactIconLink href={Links.whatsapp} src={whatsappIcon} />
         </div>
